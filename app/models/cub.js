987fc62e23009587227b08dded63194d1d5cd54d
Bobby <kolbobby@gmail.com>
2015-02-10 23:51:15 -0500

    Added getMother() and getFather() to cub model

--- app/models/cub.js
+++ app/models/cub.js
@@ -3,7 +3,8 @@
  * - APIv1
  */
 var mongoose = require('mongoose'),
-	Schema = mongoose.Schema;
+	Schema = mongoose.Schema,
+	Bear = require('./bear');
 
 var CubSchema = new Schema({
 	name: String,
@@ -13,5 +14,23 @@ var CubSchema = new Schema({
 		father: String
 	}
 });
+
+CubSchema.methods.getMother = function(cb) {
+	Bear.findById(this.parents.mother, function(err, bear) {
+		if(err)
+			res.send(err);
+
+		cb(bear);
+	});
+};
+
+CubSchema.methods.getFather = function(cb) {
+	Bear.findById(this.parents.father, function(err, bear) {
+		if(err)
+			res.send(err);
+
+		cb(bear);
+	});
+};
 
 module.exports = mongoose.model('Cub', CubSchema);
